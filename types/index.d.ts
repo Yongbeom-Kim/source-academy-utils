64d6367bdbb980aa7a14add9cdcbdcaca61998d0
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-04-09 12:51:01 +0800

    Remove .gitignore files

--- types/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { objectToLinkedList, linkedListToObject } from './object-transformer';
--- types/types.d.ts
+++
@@ -1,3 +0,0 @@
-export type LinkedList<T> = [T, LinkedList<T>] | null;
-export type AlternatingLinkedList<S, T> = [S, [T, AlternatingLinkedList<S, T>]] | null;
-export type Key = string | number | symbol;
